f2a4ba965b41d959db32306f52a18a229857d793
Ryan Hinkel <ryanhinkel@gmail.com>
2018-01-15 11:53:25 -0500

    Add pldays and remove positions; vary profit loss on different stocks

--- create_securities.py
+++ create_securities.py
@@ -3,6 +3,8 @@ import string
 import time
 
 
+BUSINESS_DAYS = 260
+
 def create_symbol():
     letters = string.ascii_uppercase
     symbol = ''
@@ -36,24 +38,34 @@ def create_positions(symbol):
 
     return positions
 
+
+def create_date_days_ago(days_ago):
+    now = time.time()
+    return now - days_ago * 60 * 60 * 24
+
 def create_security(id):
+    good_day = uniform(0.01, 0.02)
+    bad_day = uniform(-0.02, 0)
     symbol = create_symbol()
-    positions = create_positions(symbol)
-    quantity = 0
-    cost_basis = 0
-    for position in positions:
-        quantity += position['quantity']
-        cost_basis += position['original_price'] * position['quantity']
-    price = positions[-1]['original_price'] * (uniform(.8, 1.4))
-    for position in positions:
-        position['price'] = price
+    quantity = uniform(10, 150)
+    original_price = uniform(10, 50)
+    price = original_price
+    pldays = []
+    for i in range(BUSINESS_DAYS):
+        profit_loss = uniform(bad_day, good_day)
+        price *= (1 + profit_loss)
+        pldays.append({
+            'date': create_date_days_ago(BUSINESS_DAYS - i),
+            'profit_loss': profit_loss
+        })
+
     return {
         'id': id,
         'symbol': symbol,
-        'price': price,
-        'cost_basis': cost_basis,
         'quantity': quantity,
-        'positions': positions,
+        'original_price': original_price,
+        'price': price,
+        'pldays': pldays,
     }
 
 def create_securities(number):
