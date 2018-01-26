89d664c50dde863cbd3caa52f35bda2a5baa7598
Ryan Hinkel <ryanhinkel@gmail.com>
2018-01-26 15:52:38 -0500

    Lower securities

--- create_securities.py
+++ create_securities.py
@@ -136,6 +136,6 @@ MANAGERS = ['JG', 'RH', 'PS', 'DM', 'TF', 'CR', 'TC', 'RZ', 'AB', 'GS', 'MN']
 TYPES = ['EM', 'CSArb']
 BOOKS = [(manager + '-' + type_) for manager in MANAGERS for type_ in TYPES]
 REGIONS = ['APAC', 'EMEA', 'LATAM', 'NAMER']
-SECURITIES = create_securities(300)
+SECURITIES = create_securities(100)
 BOOK_INDEX = create_index(SECURITIES, 'book')
 REGION_INDEX = create_index(SECURITIES, 'region')
